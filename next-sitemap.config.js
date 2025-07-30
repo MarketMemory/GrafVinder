cab754444aa6825f815ad88aedf07240815b3e75
misstandmelder <214885088+MarketMemory@users.noreply.github.com>
2025-07-30 13:15:21 +0200

    Add next-sitemap config

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://grafvinder.vercel.app',
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+  outDir: './public',  // Voeg deze regel toe
+};
