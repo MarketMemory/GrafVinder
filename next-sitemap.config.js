4d09395ff87ec0a5eab9d13cd147ea7cb0d2d2a0
misstandmelder <214885088+MarketMemory@users.noreply.github.com>
2025-07-30 15:55:45 +0200

    Update next-sitemap config and sitemap-0.xml

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
-/** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: 'https://grafvinder.vercel.app',
   generateRobotsTxt: true,
   sitemapSize: 7000,
+  outDir: './public', // Zorg dat sitemap naar public gaat
 };
